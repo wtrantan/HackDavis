4cce681b52c2fe49924efcbd7e41ef1b155adfee
wtrantan <wtrantan@gmail.com>
2024-04-27 19:21:52 -0700

    client and server folder

---
+++ client/src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+import * as React from "react";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from './Pages/Home/Home';
+function App() {
+  return (
+     <BrowserRouter>
+      <Routes>
+      <Route path="/Home" element={<Home />} />
+      </Routes>
+    </BrowserRouter>
+
+  );
+}
+
+export default App;
