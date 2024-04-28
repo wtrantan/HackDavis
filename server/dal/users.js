e402248f3ffc3c1913c1fbfb1621f1653df9a00c
Mana <vuvictor2003@gmail.com>
2024-04-28 01:25:26 -0700

    Add getUser on DAL

--- server/dal/users.js
+++ server/dal/users.js
@@ -1,26 +1,29 @@
 const connection = require('./connection.js');
 
 async function createUser(userData) {
-    try {
+  try {
 
-        /*
-        userData = {
-            email: 'david.is.cool@gmail.com',
-            password: 'pasword123'
-        };
-        */
+    /*
+    userData = {
+        email: 'david.is.cool@gmail.com',
+        password: 'pasword123'
+    };
+    */
 
-        const result = await connection.promise().query('INSERT INTO users SET ?', userData);
-        return result.insertId;
-        console.log('Created user'); 
+    const result = await connection.promise().query('INSERT INTO users SET ?', userData);
+    return result.insertId;
+    console.log('Created user');
 
-      } catch (error) { // handles duplicate emails
-        if (error.errno === 1062) {
-          console.error('Error: Duplicate entry (1062)');
-        } else {
-          throw error;
-        }
-      }
+  } catch (error) { // handles duplicate emails
+    if (error.errno === 1062) {
+      console.error('Error: Duplicate entry (1062)');
+    } else {
+      throw error;
+    }
+  }
 }
 
-
+async function getUser(userID) {
+  const result = await connection.promise().query('SELECT * FROM users WHERE userID = ?', userID);
+  returnresult[0][0];
+}
