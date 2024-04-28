d55f66ffd738ed8eadfd8d10765f612c51ce795f
Mana <vuvictor2003@gmail.com>
2024-04-28 11:59:43 -0700

    Refactor server/dal/index.js for modularity

--- server/dal/index.js
+++ server/dal/index.js
@@ -4,10 +4,24 @@ const ingredients = require('./ingredients.js');
 const ingredientsRecipes = require('./ingredientsRecipes.js')
 
 const dal = { // dal means Data Access Layer
-  recipes,
-  users,
-  ingredients,
-  ingredientsRecipes
+  recipes: {
+    createRecipe : recipes.createRecipe, 
+    getRecipe : recipes.getRecipe, 
+    getRecipesForUser : recipes.getRecipesForUser
+  },
+  users: {
+    createUser : users.createUser, 
+    getUser : users.getUser,
+  },
+  ingredients: {
+    createIngredient : ingredients.createIngredient, 
+    getIngredient : ingredients.getIngredient, 
+    setQuantity : ingredients.setQuantity
+  },
+  ingredientsRecipes: {
+    createIngredientsRecipes : ingredientsRecipes.createIngredientsRecipes, 
+    listIngredientsForRecipe : ingredientsRecipes.listIngredientsForRecipe
+  }
 };
 
 module.exports = dal;
